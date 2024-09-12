4d099c7f6b9bfe4424a837fd6548d4b93d52c614
alexomur <alexomur111@gmail.com>
2024-09-13 00:13:58 +0300

    CLI implementation added

---
+++ Homework 1/ShellEmulator/Classes/__init__.py
@@ -0,0 +1,6 @@
+from .Command import Command
+from .Exit import Exit
+from .List import List
+from .Cd import Cd
+
+__all__ = ["Command", "Exit", "List", "Cd"]
--- Homework 1/ShellEmulator/main.py
+++ Homework 1/ShellEmulator/main.py
@@ -2,8 +2,44 @@
 I'll start with a preface that I really wanted to do the Homework in C# or something more interesting.
 But then I read 2-4 assignments and realized that Python is my choice.
 """
+import os.path, shlex, zipfile, tempfile
+
+from Classes import *
+from Configs import Config
+
+exiting: bool = False
+global_path: str
+current_path: str = ""
+
+def extract_zip():
+    with zipfile.ZipFile(Config.get_config()["path_to_zip"], "r") as zip_f:
+        temp_dir = tempfile.mkdtemp()
+        zip_f.extractall(temp_dir)
+        return temp_dir
+
 def main() -> None:
-    print("Hello world!")
+    global exiting, global_path, current_path
+
+    global_path = extract_zip()
+
+    known_commands = {
+        "ls": List(),
+        "exit": Exit(),
+        "cd": Cd(),
+    }
+
+    while not exiting:
+        line: str = input(f"{current_path}> ")
+        if len(line) == 0:
+            continue
+
+        command_name: str = line.split()[0]
+
+        args = shlex.split(''.join(line.split()[1:]))
+        if command := known_commands.get(command_name):
+            command.execute(args)
+        else:
+            print(f"Unknown command: {command_name}")
 
 if __name__ == '__main__':
     main()
