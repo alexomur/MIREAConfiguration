200c8b56c8654bea963d1936f7a03a344628e9c1
alexomur <alexomur111@gmail.com>
2024-09-12 21:16:37 +0300

    Removed unnecessary tkinter (forgor that my task is CLI project)

--- Homework 1/ShellEmulator/main.py
+++ Homework 1/ShellEmulator/main.py
@@ -2,17 +2,8 @@
 I'll start with a preface that I really wanted to do the Homework in C# or something more interesting.
 But then I read 2-4 assignments and realized that Python is my choice.
 """
-import tkinter as tk
-
-def load_window() -> tk.Tk:
-    root = tk.Tk()
-    root.title('ShellEmulator')
-    root.geometry('1200x600')
-    return root
-
 def main() -> None:
-    root = load_window()
-    root.mainloop()
+    print("Hello world!")
 
 if __name__ == '__main__':
     main()
