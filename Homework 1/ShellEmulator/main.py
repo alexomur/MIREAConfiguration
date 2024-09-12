dd46def31b5882038335109eeb8fc3f623632785
alexomur <alexomur111@gmail.com>
2024-09-12 19:29:04 +0300

    tkinter window added

--- Homework 1/ShellEmulator/main.py
+++ Homework 1/ShellEmulator/main.py
@@ -2,9 +2,17 @@
 I'll start with a preface that I really wanted to do the Homework in C# or something more interesting.
 But then I read 2-4 assignments and realized that Python is my choice.
 """
+import tkinter as tk
 
-def main():
-    pass
+def load_window() -> tk.Tk:
+    root = tk.Tk()
+    root.title('ShellEmulator')
+    root.geometry('1200x600')
+    return root
+
+def main() -> None:
+    root = load_window()
+    root.mainloop()
 
 if __name__ == '__main__':
     main()
