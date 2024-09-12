cce140f7decced4e5ede1d4c33610aa9979a0321
alexomur <alexomur111@gmail.com>
2024-09-12 18:01:00 +0300

    main.py added

---
+++ Homework 1/ShellEmulator/main.py
@@ -0,0 +1,10 @@
+"""
+I'll start with a preface that I really wanted to do the Homework in C# or something more interesting.
+But then I read 2-4 assignments and realized that Python is my choice.
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
