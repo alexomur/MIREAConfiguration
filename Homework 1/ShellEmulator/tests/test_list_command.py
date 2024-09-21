e70609c731af624b02d687c44b86a9fd108316ed
alexomur <alexomur111@gmail.com>
2024-09-21 16:10:16 +0300

    ls test completed

--- Homework 1/ShellEmulator/tests/test_list_command.py
+++ Homework 1/ShellEmulator/tests/test_list_command.py
@@ -1,15 +1,26 @@
 import unittest
-import sys
-import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from zip_handler import extract_zip
 
-from commands import list_command
-
+from commands import Command, GlobalManager
+from commands import List
 
 class TestListCommand(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+    command: Command = List()
+
+    def setUp(self):
+        GlobalManager.set_current_path("/")
+        GlobalManager.set_global_path(extract_zip("test_archive.zip"))
+        GlobalManager.set_exiting(False)
+
+    def test_standard(self):
+        self.assertEqual((True, "Test Document.txt\nTest Folder 1\nTest Folder 2"), self.command.execute([]))
+
+    def test_level2(self):
+        self.assertEqual((True, "Test Document.txt"), self.command.execute(["Test Folder 2/"]))
+
+    def test_not_existing_file(self):
+        self.assertEqual((False, "Error: Directory 'blah blah blah' does not exist."), self.command.execute(["blah blah blah"]))
 
 
 if __name__ == '__main__':
